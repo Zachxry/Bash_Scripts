c5061055b35c37ac46a6f2ddaba7893570c97e66
Zach Marmolejo <zachary.marmolejo@protonmail.com>
2021-11-21 13:18:22 -0600

    updated comments

--- subextension.sh
+++ subextension.sh
@@ -8,11 +8,14 @@ read -p "Enter hostname (Ex: google.com): " host
 echo "[+] Running Sublist3r..."
 sublist3r -d $host > sublist3r.txt
 
+# Removes color formatting from Sublist3r output
 echo "[+] Cleaning output..."
 cat sublist3r.txt | sed -r "s/\x1B\[([0-9]{1,3}(;[0-9]{1,2})?)?[mGK]//g" > cleaned.txt
 
+# grep for only the subdomains from the cleaned Sublist3r output 
 grep -o "[^/]*.$host" cleaned.txt | sort | grep -wv 'Enumerating' > list.txt
 
+# handles nslookup on the subdomains
 echo "[+] Handling subdomain lookup..."
 for domains in $(cat list.txt); do nslookup $domains >> nslookup.txt
 done
@@ -22,6 +25,5 @@ cat nslookup.txt | uniq -u > results.txt
 
 echo "[+] Cleaning up directory..."
 rm -rf cleaned.txt list.txt host.txt nslookup.txt
-
 
 echo "[+] Done!"
